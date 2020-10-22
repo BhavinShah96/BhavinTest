14e85069f8fbc0d90d08bc7828a7f6c14f2334d6
BhavinShah96 <bhavinpshah96@gmail.com>
2020-10-22 18:53:33 +0530

    My New Changes

---
+++ DotNet/HomeWork/ConsoleColorAndDateTimeApp/ConsoleColorAndDateTimeApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleColorAndDateTimeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TodayDate();
+            TextColor();
+            Console.ReadKey();
+        }
+        public static void TextColor()
+        {
+            Console.WriteLine("Bhavin Shah");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Bhavin Shah",Console.ForegroundColor);
+        }
+        public static void TodayDate()
+        {
+            Console.WriteLine(DateTime.Today.ToString("dd/MM/yyyy"));
+        }
+    }
+}
---
+++ DotNet/HomeWork/UppercaseLowercaaseLengthApp/UppercaseLowercaaseLengthApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UppercaseLowercaaseLengthApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Message = "i am learning c#";
+            string message = "I AM LEARNING C IN SWABHAV";
+
+            Uppercase(Message);
+            LowerCase(message);
+            Length(message);
+        }
+        public static void Uppercase(string Message)
+        {
+            Console.WriteLine("String in Uppercase is : " + Message.ToUpper());
+        }
+        public static void LowerCase(string message)
+        {
+            Console.WriteLine("String in Uppercase is : " + message.ToLower());
+        }
+        public static void Length(string message)
+        {
+            Console.WriteLine("String Length is : " + message.Length);
+        }
+    }
+} 
